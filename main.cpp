895c48a89d6bf094da6fbe8abf504861b1824420
Wlodzimierz Kowjako <257276@student.pwr.edu.pl>
2022-03-22 09:16:59 +0100

    Part of common multithreading logic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,67 @@
 #include <pthread.h>
 #include <ncurses.h>
+#include <stdlib.h> /* rand */
+#include <unistd.h> /* sleep */
 #include <iostream>
 
-int main()
+int THREAD_NUM;
+
+void *FindAvg(void *threadNumber)
 {
-	std::cout<<"Hello world"<<std::endl;
-    return 0;
+	std::cout<< "Wątek "<<(long)threadNumber<<" zaczyna pracę"<<std::endl;
+	int array[10];
+	for(auto i = 0; i<10; i++)
+	{
+		array[i] = rand() % 50 + 1;
+	}
+
+	int sum = 0;
+	for(auto i=0; i<10; i++)
+	{
+		sleep(rand() % 4);
+		std::cout<<"Wątek "<<(long)threadNumber<<": arr["<<i<<"] = "<<array[i]<<std::endl;
+		sum += array[i];
+	}
+
+	std::cout<<"Wątek "<<(long)threadNumber<<" wynik: "<<(double)sum / 10<<std::endl;
+	pthread_exit(NULL);
+}
+
+int main(int args, char *argv[])
+{
+	int threadCallback;;
+
+	/* Sprawdzenie poprawności uruchomienia */
+	if(args > 2)
+	{
+		std::cout<<"Należy podać tylko jeden parametr!"<<std::endl;
+		return -1;	
+	}
+
+	THREAD_NUM = (int)(*argv[1]) - 48; /* wyłuskujemy wartość spod wskaźnika */
+	pthread_t Threads[THREAD_NUM]; 
+	
+	for(auto i = 0; i<THREAD_NUM; i++)
+	{
+		threadCallback = pthread_create(&Threads[i], NULL, FindAvg, (void*)i);
+		if(threadCallback)
+		{
+			std::cout<<"Błąd podczas tworzenia wątku"<<std::endl;
+			return -1;
+		}
+	}
+
+	for(auto i = 0; i<THREAD_NUM; i++)
+	{
+		threadCallback = pthread_join(Threads[i], NULL); /* czekamy na ukończenie wątków */
+		if(threadCallback)
+		{
+			std::cout<<"Błąd podczas joinowania wątku"<<std::endl;
+			return -1;
+		}
+		std::cout<<"Poprawnie ukończony watek "<<i<<std::endl;
+	}
+
+	std::cout<<"Koniec programu..."<<std::endl;
+    pthread_exit(NULL);
 }
